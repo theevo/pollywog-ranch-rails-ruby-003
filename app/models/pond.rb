7dab10ad2165ef6a701d560532f9d1ea3bb94d06
Theo Vora <theevo@gmail.com>
2013-11-03 13:43:48 -0500

    add models and migrations for ponds, frogs, tadpoles

---
+++ app/models/pond.rb
@@ -0,0 +1,3 @@
+class Pond < ActiveRecord::Base
+  has_many :frogs
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PollywogRanchRailsRuby003::Application.config.secret_key_base = '0ab3cef0754d77e94a3f51f0f8309e473be170428d4b6bd6919f10cbe8cb8e6c329971f256f233f16f98d4e5003bd6ec8cf4cf97b967eb3eed5e4065f681026e'
