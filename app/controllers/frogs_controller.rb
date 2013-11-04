367ced1aad5207682a5f45227b50994a1c228f4b
Theo Vora <theevo@gmail.com>
2013-11-04 09:06:54 -0500

    frogs actions working

--- app/controllers/frogs_controller.rb
+++ app/controllers/frogs_controller.rb
@@ -1,2 +1,55 @@
-class FrogController < ApplicationController
+class FrogsController < ApplicationController
+  before_action :set_frog, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @frogs = Frog.all
+    'frogs#index'
+  end
+
+  def create
+    @frog = Frog.create(valid_frog_params)
+    redirect_to frogs_path
+  end
+
+  def new
+    @ponds = Pond.all
+    @frog = Frog.new
+    'frogs#new'
+  end
+
+  def show
+    'frogs#show'
+  end
+
+  def edit
+    @ponds = Pond.all
+    'frogs#edit'
+  end
+
+  def update
+    @frog.update(valid_frog_params)
+    redirect_to frogs_path
+  end
+
+  def destroy
+    if @frog.destroy
+      redirect_to frogs_path
+    else
+      "ERROR: cannot destroy frog. it's on the endagered list. HOW COULD YOU??!?!?"
+    end
+  end
+
+  # TODO: Frogs can create new tadpoles
+  # get '/frogs/:id/tadpoles/new' do
+  #   '/tadpoles/new'
+  # end
+
+  private
+    def set_frog
+      @frog = Frog.find(params[:id])
+    end
+
+    def valid_frog_params
+      params.require(:frog).permit(:name, :color, :pond_id)
+    end
 end
